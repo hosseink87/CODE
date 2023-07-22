878033ada4e4fa78a8c84ea0dd44ae0d03722302
hosseink87 <hossein_cockatiel@yahoo.com>
2023-07-22 08:06:16 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x908c414432394db14efbfb8faf292845bd121818";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
